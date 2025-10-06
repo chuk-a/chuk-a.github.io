2c0f4814d410c4809f8ca309262c5ba6ee3e6a2e
Tuvshinjargal Erdenebaatar <79018047+chuk-a@users.noreply.github.com>
2025-10-06 16:14:50 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
   organizationName: 'chuk-a',
   projectName: 'chuk-a.github.io',
+  deploymentBranch: 'main', // ← Add this line
   trailingSlash: true,
 
   i18n: {
